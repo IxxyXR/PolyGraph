bed695a705ee2bd59b4982c5b27c157e66a3f797
Andy Baker <andy@andybak.net>
2020-08-01 14:53:12 +0100

    Last changes before updating to latest mixture branch

--- Packages/com.alelievr.mixture/Runtime/Hackweek/Nodes/PolyhydraNodes/EdgeProperties .cs
+++ Packages/com.alelievr.mixture/Runtime/Hackweek/Nodes/PolyhydraNodes/EdgeProperties .cs
@@ -18,8 +18,26 @@ namespace Mixture
         [Input("Source Polyhydra")]
         public ConwayPoly sourcePoly;
 
+        public int EdgeDivisions = 2;
+
+        public bool OuterEdgesOnly;
+
         [Output("Edge Count")]
         public int EdgeCount;
+
+        [Output("Dihedral Angles")]
+        public List<float> DihedralAngles;
+
+        [Output("Points along edges")]
+        public List<Vector3> EdgePoints;
+
+        [Output("Edge Vectors")]
+        public List<Vector3> EdgeVectors;
+
+        [Output("Edge Indices")]
+        public List<int> EdgeIndices;
+
+
 
         public override bool hasPreview => false;
         public override bool showDefaultInspector => true;
